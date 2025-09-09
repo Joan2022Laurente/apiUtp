45bec9a509bf46d5d3e6ef306b76d771b92d31f4
TuNombreDeUsuario <TuCorreo@dominio.com>
2025-09-09 14:09:30 -0500

    dfsff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "http://127.0.0.1:5500" })); // Permitir todas las fuentes
 app.use(express.json());
 
 // Ruta POST para obtener eventos
